5aae433b3519ab290be111f77debd14331aaff0c
Taka Yoneda <tayoneda@yahoo.com>
2016-06-17 23:03:08 +0900

    Added MyContextListener

--- wtpweb/src/hoge/MyContextListener.java
+++ wtpweb/src/hoge/MyContextListener.java
@@ -1,19 +1,39 @@
 package hoge;
 
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+
+import com.mysql.jdbc.AbandonedConnectionCleanupThread;
 
 @WebListener
 public class MyContextListener implements ServletContextListener {
 	
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
+		Enumeration<Driver> drivers = DriverManager.getDrivers();
+		while (drivers.hasMoreElements()) {
+			Driver driver = drivers.nextElement();
+			try {
+				DriverManager.deregisterDriver(driver);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 		
+		try {
+			AbandonedConnectionCleanupThread.shutdown();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		
 	}
 }
